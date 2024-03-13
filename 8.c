00ef885da37f3559902360acf19a90e704674892
poonamshinde23 <poonamshinde1912@gmail.com>
2024-03-13 18:27:39 +0530

    8.c

---
+++ 8.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,sum,avg;
+printf("enter the number\n");
+scanf("%d %d %d",&a,&b,&c);
+sum=a+b+c;
+avg=sum/3;
+printf("average = %d\n",avg);
+return 0;
+}
