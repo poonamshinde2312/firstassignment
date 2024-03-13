becf705179e11eab988732c75d2060bfb03bb463
poonamshinde23 <poonamshinde1912@gmail.com>
2024-03-13 17:23:42 +0530

    3.c

---
+++ 3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+char n1 = 'a';
+
+printf("decimal = % d\n ", n1);
+
+printf("hex = % x\n ", n1);
+
+printf("octal = % o\n ", n1);
+
+printf("character = %c\n ", n1);
+
+return 0;
+
+
+}
+
