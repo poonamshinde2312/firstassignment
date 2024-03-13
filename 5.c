94da99a48061a7d90c89438aaad07cfa1ed7c3dc
poonamshinde23 <poonamshinde1912@gmail.com>
2024-03-13 18:14:40 +0530

    5.c

---
+++ 5.c
@@ -0,0 +1,22 @@
+/*Write a program to display ASCII values for character
+constants ‘\n’, ‘\r’, ‘\t’, ‘\a’, ‘\b’.
+*/
+#include<stdio.h>
+  
+  int main()
+
+  {
+  
+  printf("ASCII VALUE OF  \\n=%d\n",'\n');
+ 
+  printf("ASCII VALUE OF  \\r=%d\n",'\r');
+ 
+  printf("ASCII VALUE OF \\t=%d\n",'\t');
+ 
+  printf("ASCII VALUE OF \\a=%d\n",'\a');
+ 
+  printf("ASCII VALUE OF \\b=%d\n",'\b');
+ 
+  return 0;
+  }
+                              
