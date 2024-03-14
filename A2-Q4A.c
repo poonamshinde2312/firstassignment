23a85e2c7d83745d8133f24a94a4afd5f47ec6c1
poonamshinde23 <poonamshinde1912@gmail.com>
2024-03-14 18:27:19 +0530

    A2-Q4A.c

---
+++ A2-Q4A.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n1 = 20;
+int n2 = 40;
+
+if(n1>n2)
+{
+printf("n1 is greater \n");
+}
+else
+{
+printf("n2 is smaller\n");
+}
+return 0;
+}
---
+++ A2-Q4b.c
@@ -0,0 +1,20 @@
+/*Write a program to find maximum of two numbers using
+a. If – else
+b. conditional operator.
+*/
+
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the number\n");
+scanf("%d",&a);
+
+printf("enter the number\n");
+scanf("%d",&b);
+
+ a>b ? printf("a is maximum\n"):printf("b is maximum\n") ;
+
+return 0;
+}
+
